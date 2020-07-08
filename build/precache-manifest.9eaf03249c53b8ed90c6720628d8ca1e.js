98c1c8565012bf035ae30b5853143761ce58eb53
Rektsion <bentan03@gmail.com>
2020-07-08 23:36:23 +0800

    all

---
+++ build/precache-manifest.9eaf03249c53b8ed90c6720628d8ca1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f06d5edc045c07cc26fdd83308edcba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "838529d1166b24773017",
+    "url": "/static/css/main.ee21d3ee.chunk.css"
+  },
+  {
+    "revision": "97ee55b723226ca8b6bf",
+    "url": "/static/js/2.f200ff29.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f200ff29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "838529d1166b24773017",
+    "url": "/static/js/main.36ed308d.chunk.js"
+  },
+  {
+    "revision": "c5e8f5ba0b491ea9966c",
+    "url": "/static/js/runtime-main.222d136d.js"
+  }
+]);
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const bodyParser = require("body-parser")
 const cors = require("cors")
 const app = express()
 
+app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
 app.use(morgan("combined"))
